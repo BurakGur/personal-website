1bd3d343e31f6b913411680c495ccd115e39a7ba
Burak Gür <gurbrk@gmail.com>
2021-10-20 17:05:37 +0300

    added config file

---
+++ frontend/config.js
@@ -0,0 +1,2 @@
+export const wordsPerMinute = 225;
+export const url = 'https://burakgur.com/';
--- frontend/lib/helpers.js
+++ frontend/lib/helpers.js
@@ -1,5 +1,6 @@
+import { wordsPerMinute } from '../config';
+
 function timeToRead(content) {
-  const wordsPerMinute = process.env.NEXT_PUBLIC_WORDS_PER_MINUTE;
   const contentWords = content.trim().split(/\s+/);
   const wordsNumber = contentWords.length;
   return Math.ceil(wordsNumber / wordsPerMinute);
