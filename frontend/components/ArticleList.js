904a7c59f7a105f1532b2f713e4c0be08d52080c
Burak Gür <gurbrk@gmail.com>
2021-10-21 19:20:16 +0300

    added momentum package for date

--- frontend/components/ArticleList.js
+++ frontend/components/ArticleList.js
@@ -1,14 +1,17 @@
 import { timeToRead } from '../lib/helpers';
+import Moment from 'react-moment';
 
 function ArticleList({ articles }) {
   return (
     <>
       {articles.map(article => (
         <div className="my-6" key={article.id}>
-          <div className="text-2xl">{article.title}</div>
+          <div className="text-2xl font-bold text-gray-700">
+            {article.title}
+          </div>
           <div className="text-sm">
-            {article.published_at} • {timeToRead(article.content)} dakikalık
-            okuma
+            <Moment format="DD/MM/YYYY">{article.published_at}</Moment> •
+            {timeToRead(article.content)} dakikalık okuma
           </div>
           <div className="text-lg">{article.description}</div>
         </div>
