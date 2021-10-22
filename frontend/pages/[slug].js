2a1dd947071357c78d9009d17887293db7678f77
Burak Gür <gurbrk@gmail.com>
2021-10-22 16:22:39 +0300

    added blog slug

---
+++ frontend/pages/[slug].js
@@ -0,0 +1,28 @@
+import { fetchAPI } from '../lib/api';
+
+const ArticlePage = ({ article }) => {
+  return <>{article.title}</>;
+};
+
+export async function getStaticPaths() {
+  const articles = await fetchAPI('/articles');
+
+  return {
+    paths: articles.map(article => ({
+      params: {
+        slug: article.slug
+      }
+    })),
+    fallback: false
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const articles = await fetchAPI(`/articles?slug=${params.slug}`);
+  return {
+    props: { article: articles[0] },
+    revalidate: 1
+  };
+}
+
+export default ArticlePage;
