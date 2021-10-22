4a859e99244f7a9dd436db28cb6d76b1f5ed6131
Burak Gür <gurbrk@gmail.com>
2021-10-22 22:55:43 +0300

    added 404 page

---
+++ frontend/pages/404.js
@@ -0,0 +1,14 @@
+export default function ErrorPage() {
+  return (
+    <div className="my-20 text-center">
+      <div className="text-9xl font-bold">
+        <span className="text-blue-400">4</span>
+        <span className="text-pink-400">0</span>
+        <span className="text-yellow-400">4</span>
+      </div>
+      <div className="mt-4 text-xl font-medium text-gray-600">
+        Aradığınız sayfayı bulamadık.
+      </div>
+    </div>
+  );
+}
