3d9c49ffb12ed0d1db0a0b8db4a2d23c564de6f1
Burak Gür <gurbrk@gmail.com>
2021-10-22 23:02:44 +0300

    change rss settings

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,11 @@
 export const wordsPerMinute = 225;
-export const url = 'https://burakgur.com/';
+export const url = 'https://burakgur.com';
 export const gtmId = 'UA-59811767-1';
 export const locale = 'tr';
+export const author = {
+  name: 'Burak Gür',
+  email: 'gurbrk@gmail.com'
+};
+export const title = 'burak gür.';
+export const description =
+  "Burak Gür'ün Kişisel Blog sitesidir. JavaScript, motivasyon ve kişisel gelişimle ilgilidir.";
--- frontend/lib/rss.js
+++ frontend/lib/rss.js
@@ -1,20 +1,17 @@
 import { Feed } from 'feed';
 import fs from 'node:fs/promises';
+import { author, url, title, description } from '../config';
 
 const generateRSS = async blogs => {
   const feed = new Feed({
-    title: "Burak Gür'ün Kişisel Blogu - RSS Feed",
-    description:
-      "Burak Gür'ün Kişisel Blog Feed'idir. Yazılımla ve kitaplarla ilgilidir.",
-    id: 'https://burakgur.com/',
-    link: 'https://burakgur.com/',
+    title: `${title} - RSS Feed`,
+    description: description,
+    id: url,
+    link: url,
     language: 'tr',
-    favicon: 'https://burakgur.com/favicon.ico',
+    favicon: `${url}/favicon.ico`,
     generator: 'Next.js',
-    author: {
-      name: 'Burak Gür',
-      email: 'gurbrk@gmail.com'
-    }
+    author: author
   });
 
   for (const post of blogs) {
@@ -24,7 +21,7 @@ const generateRSS = async blogs => {
       link: post.url,
       description: post.description,
       content: post.content,
-      date: new Date(post.created_at)
+      date: new Date(post.published_at)
     });
   }
   fs.writeFileSync('./public/rss.xml', feed.rss2());
