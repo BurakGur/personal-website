dd01853e486b50d4fd846826e4b39f9f3f0a8831
Burak Gür <gurbrk@gmail.com>
2021-10-26 23:22:41 +0300

    changed tailwind mode

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,24 +1,25 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {},
     fontFamily: {
-      sans: ['Inter', 'sans-serif'],
+      sans: ['Inter', 'sans-serif']
     },
-    container: (theme) => ({
+    container: theme => ({
       center: true,
       padding: theme('spacing.4'),
       screens: {
         sm: '100%',
         md: '100%',
         lg: '100%',
-        xl: '720px',
-      },
-    }),
+        xl: '720px'
+      }
+    })
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [require('@tailwindcss/typography')],
-}
+  plugins: [require('@tailwindcss/typography')]
+};
