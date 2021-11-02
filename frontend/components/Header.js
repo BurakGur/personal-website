6bfdc16e01db2234c8052fb49ac4db22ebced6da
Burak Gür <gurbrk@gmail.com>
2021-11-02 23:04:13 +0300

    added toggle button for open menu

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,16 +1,20 @@
 import Link from 'next/link';
 import { title } from '../config';
 import { Bars } from '../icons';
+import { useKBar } from 'kbar';
 
 function Header() {
+  const { query } = useKBar();
   return (
     <header className="flex justify-between items-center my-10">
       <div className="text-3xl font-bold text-indigo-500">
         <Link href="/">{title}</Link>
       </div>
       <button
+        onClick={() => query.toggle()}
         type="button"
-        className="text-gray-400 hover:text-gray-500 transition-colors duration-200"
+        className="text-gray-400 hover:text-gray-500
+        transition-colors duration-200"
       >
         <Bars
           className="transition-colors duration-200"
