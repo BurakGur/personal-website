ed8e469634b00e0deb320cb59a0fee66f9c11050
Burak Gür <gurbrk@gmail.com>
2021-11-02 22:48:18 +0300

    Removed cmd k label to header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -10,16 +10,13 @@ function Header() {
       </div>
       <button
         type="button"
-        className="group leading-6 font-bold flex items-center space-x-3 text-gray-400 hover:text-gray-500 transition-colors duration-200 py-2"
+        className="text-gray-400 hover:text-gray-500 transition-colors duration-200"
       >
         <Bars
           className="transition-colors duration-200"
           width="22"
           height="22"
         />
-        <span className="duration-200 text-xs leading-5 py-0.5 px-1.5 border border-gray-400 rounded">
-          <kbd className="font-sans">CMD + K</kbd>
-        </span>
       </button>
     </header>
   );
