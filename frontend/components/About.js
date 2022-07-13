2406ccdbb63e478579e09b842595a9c411078f95
Burak Gür <gurbrk@gmail.com>
2022-07-13 20:27:43 +0000

    changed about component old version

--- frontend/components/About.js
+++ frontend/components/About.js
@@ -1,19 +1,14 @@
-import Image from 'next/image';
-
 function About() {
   return (
     <div className="flex items-center bg-gray-200 rounded-xl p-3 md:p-6 md:flex-row flex-col">
-      <div className="w-28 h-28 hidden md:flex">
-        <Image
-          className="rounded-full"
-          src="/burak-gur.jpeg"
-          alt="Burak Gür"
-          layout="fixed"
-          width={112}
-          height={112}
-        />
-      </div>
-      <div className="p-2 text-gray-600 leading-6 md:px-4 md:p-0">
+      <img
+        className="rounded-full"
+        src="/burak-gur.jpeg"
+        alt="Burak Gür"
+        width="112"
+        height="112"
+      />
+      <div className="px-4 text-gray-600 leading-6">
         <p className="font-bold">Merhaba, ben Burak.</p>
         <p>
           Frontend Developer olarak çalışıyorum. JavaScript, motivasyon ve
