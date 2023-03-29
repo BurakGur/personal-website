5350e2eb14dbb5edbba9a2ff926481e4e0d8a068
burakgur <gurbrk@gmail.com>
2023-03-29 15:23:52 +0300

    added vercel analytics

--- app/providers.js
+++ app/providers.js
@@ -3,11 +3,13 @@
 import { ThemeProvider } from 'next-themes';
 import NextNProgress from 'nextjs-progressbar';
 import { primaryColor } from 'config';
+import { Analytics } from '@vercel/analytics/react';
 
 export function Providers({ children }) {
   return (
     <ThemeProvider attribute="class">
       <NextNProgress color={primaryColor} height={2} />
+      <Analytics />
       {children}
     </ThemeProvider>
   );
