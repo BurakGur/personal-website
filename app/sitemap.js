1bdca1a6c37616b464fe468fda29ec51aa439b78
burakgur <gurbrk@gmail.com>
2023-04-06 11:48:09 +0300

    sitemap test

---
+++ app/sitemap.js
@@ -0,0 +1,17 @@
+import { allPosts } from 'contentlayer/generated';
+import { url } from 'config';
+import { navItems } from '../utils/constants';
+
+export default async function sitemap() {
+  const posts = allPosts.map(post => ({
+    url: `${url}/blog/${post.slug}`,
+    lastModified: post.date.split('T')[0]
+  }));
+
+  const pages = Object.keys(navItems).map(page => ({
+    url: `${url}${page}`,
+    lastModified: new Date().toISOString().split('T')[0]
+  }));
+
+  return [...pages, ...posts];
+}
