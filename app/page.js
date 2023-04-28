a929e18e8197ee9b2be122d21f4060b4e53301f7
burakgur <gurbrk@gmail.com>
2023-04-28 23:23:26 +0300

    added prettier tailwind plugin

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 export default async function Home() {
   return (
     <section>
-      <h1 className="font-medium text-gray-900 mb-4 font-serif dark:text-gray-0 text-3xl">
+      <h1 className="mb-4 font-serif text-3xl font-medium text-gray-900 dark:text-gray-0">
         Merhaba, ben Burak.
       </h1>
-      <p className="text-gray-400 dark:text-gray-200 mb-4">
+      <p className="mb-4 text-gray-400 dark:text-gray-200">
         Frontend Developer olarak çalışıyorum. JavaScript, motivasyon ve kişisel
         gelişim ile ilgili blog yazılarımı buradan okuyabilirsiniz. Ayrıca bu
         web sitesi de dahil tüm açık kaynak projelerimi GitHub üzerinden
